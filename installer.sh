6cde8415310c3983db398e2f0a464b0eece09973
Michael Ruppe <michael.ruppe@live.com.au>
2017-04-21 15:38:19 +1000

    Prototype: do not use yet
    Prototype installer script automates a few steps:
    Installs necessary packages and makes changes to /boot/config.txt to
    enable camera interface. The aim is to streamline setup on a fresh install of
    raspbian jessie lite

---
+++ installer.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+# installer.sh will install the necessary packages to get the gifcam up and running.
+# If you want Samba shared folders or USB drive support you will have to follow the instructions on
+# https://github.com/CoreElectronics/gifcam
+
+# Packages to install
+PACKAGES="python-picamera graphicsmagick python-pip"
+
+apt-get update
+apt-get upgrade -y
+apt-get install $PACKAGES -y
+pip install twython
+
+
+# Enable camera interface if it is not already.
+if grep "start_x=1" /boot/config.txt
+then
+	break
+else
+	sed -i "s/start_x=0/start_x=1/g" /boot/config.txt
+fi
+
+# Camera requires 128MB GPU memory. Search for the gpu_mem parameter and replace the line.
+if grep "gpu_mem=128" /boot/config.txt
+then
+	break
+else
+	sed -i "/gpu_mem/c\gpu_mem=128" /boot/config.txt
+fi
+
+echo "Install complete, rebooting."
+reboot
