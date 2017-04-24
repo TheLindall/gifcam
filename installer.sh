707ffc335a7e1024af0977aad6664d3f19ccaece
Michael Ruppe <michael.ruppe@live.com.au>
2017-04-24 10:58:18 +1000

    Fix installer.sh behaviour
    installer now treats /boot/config.txt correctly, handling line
    modification and appending if necessary. Unsure if the execute
    permission will be retained on clone.

--- installer.sh
+++ installer.sh
@@ -1,34 +1,42 @@
 #!/bin/sh
-# installer.sh will install the necessary packages to get the gifcam up and running.
-# If you want Samba shared folders or USB drive support you will have to follow the instructions on
-# https://github.com/CoreElectronics/gifcam
+# installer.sh will install the necessary packages to get the gifcam up and running with 
+# basic functions
 
-# Packages to install
+# Install packages
 PACKAGES="python-picamera graphicsmagick python-pip"
-
 apt-get update
 apt-get upgrade -y
 apt-get install $PACKAGES -y
 pip install twython
 
-###########
-# TODO: fix - camera parameters aren't in boot.txt to begin with, so following code doesn't work
-###########
 
-# Make sure camera interface is enabled
-if grep "start_x=1" /boot/config.txt
+## Enable Camera Interface
+CONFIG="/boot/config.txt"
+
+# If a line containing "start_x" exists
+if grep -Fq "start_x" $CONFIG
 then
-	break
+	# Replace the line
+	echo "Modifying start_x"
+	sed -i "s/start_x=0/start_x=1/g" $CONFIG
 else
-	sed -i "s/start_x=0/start_x=1/g" /boot/config.txt
+	# Create the definition
+	echo "start_x not defined. Creating definition"
+	echo "start_x=1" >> $CONFIG
 fi
-# Camera requires 128MB GPU memory. Search for the gpu_mem parameter and replace the line.
-if grep "gpu_mem=128" /boot/config.txt
+
+# If a line containing "gpu_mem" exists
+if grep -Fq "gpu_mem" $CONFIG
 then
-	break
+	# Replace the line
+	echo "Modifying gpu_mem"
+	sed -i "/gpu_mem/c\gpu_mem=128" $CONFIG
 else
-	sed -i "/gpu_mem/c\gpu_mem=128" /boot/config.txt
+	# Create the definition
+	echo "gpu_mem not defined. Creating definition"
+	echo "gpu_mem=128" >> $CONFIG
 fi
+
 
 echo "Install complete, rebooting."
 reboot
